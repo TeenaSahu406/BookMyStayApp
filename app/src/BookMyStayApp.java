34d7eacbbff475f05b1ddd5ed07d96e9d9d45afb
TeenaSahu406 <teenasahu0406@gmail.com>
2026-03-25 14:05:56 +0530

    UC3- Centralized Room Inventory Management

--- app/src/BookMyStayApp.java
+++ app/src/BookMyStayApp.java
@@ -1,59 +1,45 @@
-abstract class Room {
-    String type;
-    int beds;
-    double price;
+import java.util.HashMap;
 
-    Room(String type, int beds, double price) {
-        this.type = type;
-        this.beds = beds;
-        this.price = price;
+class RoomInventory {
+
+    HashMap<String, Integer> rooms;
+
+    RoomInventory() {
+        rooms = new HashMap<>();
+
+        // initialize room availability
+        rooms.put("Single Room", 5);
+        rooms.put("Double Room", 3);
+        rooms.put("Suite Room", 2);
     }
 
-    void displayRoom() {
-        System.out.println("Room Type: " + type);
-        System.out.println("Beds: " + beds);
-        System.out.println("Price: " + price);
+    void showInventory() {
+        System.out.println("Room Inventory:");
+        for (String room : rooms.keySet()) {
+            System.out.println(room + " : " + rooms.get(room));
+        }
     }
-}
 
-class SingleRoom extends Room {
-    SingleRoom() {
-        super("Single Room", 1, 2000);
+    int getAvailability(String roomType) {
+        return rooms.getOrDefault(roomType, 0);
     }
-}
 
-class DoubleRoom extends Room {
-    DoubleRoom() {
-        super("Double Room", 2, 3500);
-    }
-}
-
-class SuiteRoom extends Room {
-    SuiteRoom() {
-        super("Suite Room", 3, 6000);
+    void updateAvailability(String roomType, int count) {
+        rooms.put(roomType, count);
     }
 }
 
 public class BookMyStayApp {
+
     public static void main(String[] args) {
 
-        Room single = new SingleRoom();
-        Room doubleRoom = new DoubleRoom();
-        Room suite = new SuiteRoom();
-
-        int singleAvailable = 5;
-        int doubleAvailable = 3;
-        int suiteAvailable = 2;
+        RoomInventory inventory = new RoomInventory();
 
         System.out.println("Book My Stay - Hotel Booking System \n");
 
-        single.displayRoom();
-        System.out.println("Available: " + singleAvailable + "\n");
+        inventory.showInventory();
 
-        doubleRoom.displayRoom();
-        System.out.println("Available: " + doubleAvailable + "\n");
-
-        suite.displayRoom();
-        System.out.println("Available: " + suiteAvailable);
+        System.out.println("\nChecking availability of Single Room:");
+        System.out.println(inventory.getAvailability("Single Room"));
     }
 }
